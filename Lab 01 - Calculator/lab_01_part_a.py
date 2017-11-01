1bbff7b47b483fe28ce1c38390b8c81aec06a0c8
BradyBallmann <bballmann@stcharlessd.org>
2017-11-01 12:15:07 -0500

    I made the program

--- Lab 01 - Calculator/lab_01_part_a.py
+++ Lab 01 - Calculator/lab_01_part_a.py
@@ -1,1 +1,10 @@
-#
+#!/usr/bin/env/ python3
+# Calculator Part a
+# Brady Ballmann
+# 11/01/2017
+
+fahrenheit = int(input("Enter a tempature in Fahrenheit: "))
+
+celcius = (fahrenheit - 32) * 5/9
+
+print(celcius)
