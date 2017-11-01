be67cb756e656beb580f4485a1b208d02ebcd232
BethanyDuMontelle <bdumontelle@stcharlessd.org>
2017-11-01 09:14:31 -0500

    Completed lab A Chapter 1

--- Lab 01 - Calculator/lab_01_part_a.py
+++ Lab 01 - Calculator/lab_01_part_a.py
@@ -1,1 +1,8 @@
-#
+#!/usr/bin/env python3
+# 1.1 Lab Part A
+# Bethany DuMontelle
+#1 November 2017
+
+fahrenheit_temp = int(input('Enter temperature in Fahrenheit: '))
+celsius_temp = ( ((fahrenheit_temp) - 32) / 1.8 )
+print(celsius_temp)
