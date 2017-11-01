54b4b2470259544208640fd7d03fece720c5698c
Jrhod99 <jarhodes@stcharlessd.org>
2017-11-01 09:25:34 -0500

    Finished Lab01 programs a,b,c

--- Lab 01 - Calculator/lab_01_part_b.py
+++ Lab 01 - Calculator/lab_01_part_b.py
@@ -1,1 +1,16 @@
-#
+#!/usr/bin/env python3
+# lab_01_part_b.py
+# Jared Rhodes
+# 11/01/2017
+
+""" This is the second part of Chapter 1 Lab Pygame """
+
+print("Finding the Area of a Trapezoid")
+
+height = int(input("Enter the height of the trapezoid: "))
+base_One = int(input("Enter the length of the bottom base: "))
+base_Two = int(input("Enter the length of the top base: "))
+
+area = ((base_One + base_Two) * height) / 2
+
+print('The area is:', area)
