b57c8049cfa99d5840ae3f934997f0f49d0a2295
RyanRosvall <rrosvall@stcharlessd.org>
2017-11-01 09:10:06 -0500

    completed chapter 1

--- Lab 01 - Calculator/lab_01_part_c.py
+++ Lab 01 - Calculator/lab_01_part_c.py
@@ -1,1 +1,8 @@
-#
+#!/usr/bin/env python3
+#Circle Area Calculator
+#Ryan Rosvall
+#11/1/17
+
+number = int(input('Enter the radius: '))
+calculation = (3.14) * number ** 2
+print('Area of circle: ' + str(calculation))
