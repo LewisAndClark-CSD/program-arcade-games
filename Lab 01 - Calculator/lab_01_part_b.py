95dd0ce7cc2242f92a493f9083207d9de091428c
ryamoore <rymoore@stcharlessd.org>
2017-11-02 12:41:29 -0500

    lab 1 part b

--- Lab 01 - Calculator/lab_01_part_b.py
+++ Lab 01 - Calculator/lab_01_part_b.py
@@ -1,1 +1,6 @@
-#
+#area of trapezoid
+height = int(input("Enter the height of the trapezoid: "))
+bot = int(input("Enter length of the bottom base: "))
+top = int(input("Enter length of the top base: "))
+trap = ((bot + top)/2)*height
+print ("The area is:",trap)
