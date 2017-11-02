9139475bf9b8070fc8dd4eea45c8018cb96ba402
Conner Walkenhorst <33132628+ConnerW1719@users.noreply.github.com>
2017-11-02 13:01:40 -0500

    Update lab_01_part_a.py

--- Lab 01 - Calculator/lab_01_part_a.py
+++ Lab 01 - Calculator/lab_01_part_a.py
@@ -1,1 +1,9 @@
-#
+#!/usr/bin/env python3
+# 1.1 Part A
+# Conner Walkenhorst
+# 11/2/2017
+"""ask the user for a temperature in Fahrenheit, then print the temperature in Celsius"""
+
+temperature = int(input('Enter a temperature in Fehrenheit: '))
+celsius = ((temperature - 32)/1.8)
+print(celsius)
