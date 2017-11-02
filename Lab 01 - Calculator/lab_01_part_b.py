f8f46f73b20c944b84ab25433609bd725beda545
ChristianGroves <cgroves@stcharlessd.org>
2017-11-02 07:59:36 -0500

    labs completeddddd

--- Lab 01 - Calculator/lab_01_part_b.py
+++ Lab 01 - Calculator/lab_01_part_b.py
@@ -1,1 +1,9 @@
-#
+#!/usr/bin/env python3
+# Calculator part a
+# Christian Groves
+# 11-1-17
+print('Area of a trapezoid')
+height = int(input('Enter the height of the trapezoid: '))
+bottombase = int(input('Enter the length of the botom base: '))
+topbase = int(input('Enter the length of the top base: '))
+print('The area is: ' + str((1/2)*(bottombase + topbase) * height))
--- Lab 01 - Calculator/lab_01_part_c.py
+++ Lab 01 - Calculator/lab_01_part_c.py
@@ -1,1 +1,8 @@
-#
+#!/usr/bin/env python3
+# Calculator part a
+# Christian Groves
+# 11-1-17
+import math
+print('Area of a Pentagon')
+sidelen = ('Enter side lenght')
+print('Area of a pentagon is: ' + (5(5+2 5**()**(1/40))
