95be4ba1b9aaafc4f1ca1c9bf7e59d21d413c4dd
jriebow <jriebow@stcharlessd.org>
2017-11-02 13:36:20 -0500

    Completed Chapter 1

--- Lab 01 - Calculator/lab_01_part_b.py
+++ Lab 01 - Calculator/lab_01_part_b.py
@@ -1,1 +1,13 @@
-#
+#!/usr/bin/env python3
+#lab_01_part_b.py
+#Justin Riebow
+#11/2/17
+
+"""Prints area of trapezoid"""
+
+print("Area of a trapezoid")
+trapezoidHeight = int(input("Enter the height of the trapezoid: "))
+trapezoidBottomLength = int(input("Enter the length of the bottom base: "))
+trapezoidTopLength = int(input("Enter the lenght of the top base: "))
+trapezoidArea = (0.5 * (trapezoidBottomLength + trapezoidTopLength) * trapezoidHeight)
+print("The area is:", (trapezoidArea))
--- Lab 01 - Calculator/lab_01_part_c.py
+++ Lab 01 - Calculator/lab_01_part_c.py
@@ -1,1 +1,13 @@
-#
+#!/usr/bin/env python3
+#lab_01_part_c.py
+#Justin Riebow
+#11/2/17
+
+"""Finds the area of a circle"""
+
+from math import pi
+print("Area of a circle")
+circleRadius = int(input("Enter the radius of the circle: "))
+circleArea = pi*circleRadius**2
+print(circleArea)
+    
