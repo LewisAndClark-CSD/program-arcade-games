4e3d951b7dab0c2bb2e21de1c8d7106466759a7f
EvanWitterholt <33132584+EvanWitterholt@users.noreply.github.com>
2017-11-02 12:50:04 -0500

    Update lab_01_part_b.py

--- Lab 01 - Calculator/lab_01_part_b.py
+++ Lab 01 - Calculator/lab_01_part_b.py
@@ -1,1 +1,10 @@
-#
+#!/usr/bin/env python3
+# Finding the area of a trapezoid
+# Evan Witterholt
+#11/2/2017
+height = int(input("Enter the height of the trapezoid: "))
+length = int(input("Enter the length of the bottom base: "))
+length2= int(input("Enter the length of the top base : "))
+part1 = length + length2
+part2 = height * part1 * 0.5
+print(part2)
