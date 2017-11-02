526fa8ed76c476e51e48eb7553a2eac0348417f2
LaurenBoehme <LBOEHME@stcharlessd.org>
2017-11-02 10:05:30 -0500

    Chapter 1 Completed

--- Lab 01 - Calculator/lab_01_part_c.py
+++ Lab 01 - Calculator/lab_01_part_c.py
@@ -1,1 +1,9 @@
-#
+#!/usr/bin/env python3
+# Lab 01
+# Lauren Boehme
+# 11/1/17
+
+PI = int(3.14159)
+radius=int(input('What is the radius of the circle? ')
+circle_rad = PI * (radius)**2
+print(circle_rad)
