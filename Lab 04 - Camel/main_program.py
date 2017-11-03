b5d188ee3c51336bfb9463e9d2650c42e009e046
Sthomp123 <sthompson@stcharlessd.org>
2017-11-03 13:18:05 -0500

    Added comments to Lab 04

--- Lab 04 - Camel/main_program.py
+++ Lab 04 - Camel/main_program.py
@@ -1,10 +1,18 @@
-#
+#!/usr/bin/env python3
+# main_program.py
+# Steven Thompson
+# November 3rd 2017
+
+""" This is a game about making your way across the desert on a stolen camel while trying to outrun the natives"""
+
+
 import random
 
 print("Welcome to Camel!")
 print("You have stolen a camel to make your way across the great Mobi desert.")
 print("The natives want their camel back and are chasing you down!" "\n" "Survive your desert trek and out run the natives.")
 
+#These variables are the base mechanics of the game.
 total_miles_traveled = 0
 miles_traveled = 0
 thirst = 0
@@ -12,21 +20,24 @@ camel_tiredness = 0
 drinks_in_canteen = 3
 native_location = -20
 
+#This is what keeps the game going. If it gets set to true, then the game ends.
 done = False
 
 while done == False:
+#these are possible choices for your character
     print("A. Drink from your canteen.")
     print("B. Ahead moderate speed.")
     print("C. Ahead full speed.")
     print("D. Stop for the night.")
     print("E. Status check.")
     print("Q. Quit.")
-
+#this line allows you to select a choice from above.
     user_choice = input("Your choice? ")
-    if user_choice.upper() == "Q":
+ #If you select Q then the game ends.
+ if user_choice.upper() == "Q":
         done = True
 
-
+#If You select E then it will show you all the necessary stats to get you across the desert safely
     elif user_choice.upper() == "E":
        print("Miles Traveled:", total_miles_traveled)
        print("Drinks in canteen:", drinks_in_canteen)
@@ -34,7 +45,7 @@ while done == False:
        print("Camel tiredness:", camel_tiredness)
        print("Thirst", thirst)
        
-
+#If you select D then the tiredness of your camel is reset and the natives move in closer on your location
     elif user_choice.upper() == "D":
         camel_tiredness = 0
         print("The camel is happy")
@@ -47,7 +58,7 @@ while done == False:
         if -8 <= native_location <= -2:
             print("The natives are getting really close!")
         
-
+#If you select C then you will move a random integer between 10 and 20 forward and there is also a 1 in 20 chance of finding an oasis
     elif user_choice.upper() == "C":
         miles_traveled += random.randint(10,20)
         print("You traveled",miles_traveled,"miles")
@@ -84,7 +95,7 @@ while done == False:
             camel_tiredness = 0
             drinks_in_canteen = 4
 
-
+#If you select B then you will move forward a random integer between 5 and 12. This also has a 1 in 20 chance of finding an oasis
     elif user_choice.upper() == "B":
         miles_traveled += random.randint(5,12)
         print("You traveled",miles_traveled,"miles.")
@@ -121,6 +132,7 @@ while done == False:
             camel_tiredness = 0
             drinks_in_canteen = 4
 
+#if you select A then you will take a drink from your canteen
     elif user_choice.upper() == "A":
         if drinks_in_canteen > 0:
             drinks_in_canteen = drinks_in_canteen - 1
