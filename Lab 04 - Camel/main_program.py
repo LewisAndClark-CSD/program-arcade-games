64be215a162885bd479f6983124b89d30c597ca7
BethanyDuMontelle <bdumontelle@stcharlessd.org>
2017-11-07 08:43:46 -0600

    completed chapter 4

--- Lab 04 - Camel/main_program.py
+++ Lab 04 - Camel/main_program.py
@@ -5,37 +5,42 @@
 
 from random import randint
 
-miles_left = 1000
-natives_range = 1025
-user_thirst = 6
-canteen = 5
-camel_rest = 10
+#sets up the variables needed
+miles_left = 0
+natives_range = -20
+user_thirst = 0
+canteen = 3
+camel_rest = 0
 done = False
 
+#Introduces the player
 print('Welcome to Camel!')
 print('You have stolen a camel to make your way across the Great Mobi desert.')
 print('The natives want their camel back and are chasing you down! Survive')
 print('your desert trek and out run the natives.')
 
+#begins the loop
 while done != True:
+    #tells the player how close they are, stops negative numbers from showing
+    native_close = (miles_left) - (natives_range)
     
-    native_close = (natives_range) - (miles_left)
-    
-    oasis = randint(1,20) 
+    #sets up if an oasis can be found every time the loop starts over
+    oasis = randint(1,15) 
         
-    print('You are', str(miles_left), 'miles from crossing the Mobi dessert.')
+    #tells the player important information
+    print('You have traveled', str(miles_left), 'miles.')
     
-    if native_close > 0 and native_close < 20:
-        print('The natives are only', str(native_close), 'miles from you!')
+    if native_close < 15:
+        print('The natives getting close!')
     else:
         print('The Natives are', str(native_close), 'miles away from you.')
         
     print('You have', str(canteen), 'drinks left in your canteen.')
     
-    if user_thirst <= 2:
-        print('You are getting thirsty!')
+    if user_thirst >= 4:
+        print('You are thirsty!')
         
-    if camel_rest <= 2:
+    if camel_rest >= 5:
         print('The camel is getting tired!')
         
     
@@ -49,69 +54,85 @@ while done != True:
     
     user_choice = input()
     
+    #allows the player to drink from their canteen
+    #also prevents players from drinking water they don't have
     if user_choice.lower() == 'a':
         if canteen <= 0:
             print('You don\'t have any more water!')
         else:
-            user_thirst = user_thirst + 1
+            user_thirst = user_thirst - 1
             canteen = canteen - 1
         
+    #moves the player, also can find an oasis if the random number was 1
     elif user_choice.lower() == 'b':
-        miles_move = randint(11,29)
-        miles_left = (miles_left) - (miles_move)
-        natives_move = randint(15,24)
-        natives_range = (natives_range) - (natives_move)
-        camel_rest = (camel_rest) - 1
-        user_thirst = user_thirst - 1
+        miles_move = randint(5,12)
+        miles_left = (miles_left) + (miles_move)
+        natives_move = randint(7,14)
+        natives_range = (natives_range) + (natives_move)
+        camel_rest = (camel_rest) + 1
+        user_thirst = user_thirst + 1
         print('You moved', + (miles_move), 'miles.')
         if oasis == 1:
             canteen_fill = randint(2,5)
+            canteen = (canteen) + (canteen_fill)
+            user_thirst = user_thirst + 2
             print('You found an oasis!')
             print('You can now drink', (canteen_fill), 'more times.')        
-        
+    
+    #also moves the player, but with more random elements and an oasis can
+    #be found as well
     elif user_choice.lower() == 'c':
-        miles_move = randint(25,36)
-        miles_left = (miles_left) - (miles_move)
-        natives_move = randint(15,24)
-        natives_range = (natives_range) - (natives_move)
-        camel_rest = (camel_rest) - 2
-        user_thirst = user_thirst - 1
+        miles_move = randint(10,20)
+        miles_left = (miles_left) + (miles_move)
+        natives_move = randint(7,14)
+        natives_range = (natives_range) + (natives_move)
+        camel_tired = randint(1,3)
+        camel_rest = (camel_rest) + (camel_tired)
+        user_thirst = user_thirst + 1
         print('You moved', + (miles_move), 'miles.')
         if oasis == 1:
             canteen_fill = randint(2,5)
+            canteen = (canteen) + (canteen_fill)
+            user_thirst = user_thirst + 2
             print('You found an oasis!')
             print('You can now drink', (canteen_fill), 'more times.')        
         
+    #sets camel_rest to 0, moves the natives up as well
     elif user_choice.lower() == 'd':
-        camel_shleep = randint(4,10)
-        camel_rest = (camel_rest) + (camel_shleep)
-        natives_move = randint(15,20)
-        natives_range = (natives_range) - (natives_move)
+        camel_rest = 0
+        natives_move = randint(7,14)
+        natives_range = (natives_range) + (natives_move)
         print('The camel is happy.')
     
+    #gives the player more stats
     elif user_choice.lower() == 'e':
-        print('Miles left:', str(miles_left))
-        print('Native distance:', str(natives_range))
+        print('Miles left:', 200 - (miles_left))
+        print('Native distance:', (native_close))
         print('Canteen drinks:', str(canteen))
-        print('Your thirst:', str(user_thirst))
-        print('Camel rest level:', str(camel_rest))
+        print('Days until water is needed:', 6 - (user_thirst))
+        print('Camel tiredness:', (camel_rest), 'out of 8')
         
+    #ends the game
     elif user_choice.lower() == 'q':
         print('You gave up. Your family is disappointed in you.')
         done = True
         
-    if miles_left <= 0:
+    #winning conditon
+    if miles_left >= 200:
         print('You made it across the Mobi desert! Congradulations!')
         done = True
-            
-    if user_thirst <= 0:
+          
+    #way to lose 
+    if user_thirst >= 6:
         print('You died of thirst! Game over!')
         done = True
-            
-    if camel_rest <= 0:
+         
+    #another way to lose   
+    if camel_rest >= 8:
         print('Your camel died! Game over!')
         done = True
-        
-    if native_close <= 0:
+       
+    #yet another way to lose 
+    if natives_range >= miles_left:
         print('The Natives got you! Game over!')
         done = True
