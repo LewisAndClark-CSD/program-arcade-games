19dd9ae06b520351dac36f9b293025e464f362b7
Jake Faucett <jfaucett@PC02.CIS.COM>
2017-11-07 12:58:19 -0600

    Finished labs

--- Lab 01 - Calculator/lab_01_part_a.py
+++ Lab 01 - Calculator/lab_01_part_a.py
@@ -1,1 +1,19 @@
 #
+#!/usr/bin/env.python3
+#Converting Fahrenheit to Celsius
+#Jake Faucett
+#11-7-17
+
+
+
+
+
+tempF = int(input('Enter temperature in Fahrenheit: '))
+
+#Converting Fahrenhiet to Celcuis
+
+tempC = (tempF - 32) * (5/9)
+
+#Print this to the console
+
+print('The temperature in Celcuis: ', tempC)
--- Lab 01 - Calculator/lab_01_part_b.py
+++ Lab 01 - Calculator/lab_01_part_b.py
@@ -1,1 +1,17 @@
 #
+
+#!/usr/bin/env.python3
+#Converting Fahrenheit to Celsius
+#Jake Faucett
+#11-7-17
+
+#Find the Area of a Trapezoid
+
+height = int(input('Enter the height of the trapezoid: '))
+lengthB = int(input('Enter the length of the bottom base: '))
+lengthT = int(input('Enter the length of the top base: '))
+
+#Calculate the area
+Area = 0.5 * (lengthB + lengthT) * height
+
+print ('The area is: ', Area)
