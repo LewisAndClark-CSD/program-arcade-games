ec479a19a339b36d4fe9f6ccb66fb52b07cdc80b
BethanyDuMontelle <bdumontelle@stcharlessd.org>
2017-11-08 10:12:35 -0600

    Finished worksheet 5 and started lab 5

--- Lab 05 - Create a Picture/main_program.py
+++ Lab 05 - Create a Picture/main_program.py
@@ -1,1 +1,28 @@
-#
+#!/usr/bin/env python3
+#Lab 5 Draw a Picture
+#Bethany DuMontelle
+#8 November 2017
+
+#Starts up pygame
+import pygame
+pygame.init()
+
+#Establishes colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+LIGHT_GREEN = (74, 224, 129)
+
+size = (800, 800)
+screen = pygame.display.set_mode(size)
+
+done = False
+while not done:
+    
+    pygame.draw.ellipse(screen, LIGHT_GREEN, [50, 50, 40, 40], 5)
+    
+    pygame.display.update()
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: 
+            done = True
+pygame.quit()
