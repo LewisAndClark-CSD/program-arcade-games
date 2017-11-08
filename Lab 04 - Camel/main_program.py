f122a9c4ea04e14f138a7e1ab08db797db921e1e
EvanWitterholt <33132584+EvanWitterholt@users.noreply.github.com>
2017-11-08 11:38:49 -0600

    Update main_program.py

--- Lab 04 - Camel/main_program.py
+++ Lab 04 - Camel/main_program.py
@@ -1,1 +1,18 @@
 #
+import random
+
+cheese = 1
+pepperoni = 2
+supreme = 3
+
+timepassed = 0
+
+number = random.randint(0,3)
+
+
+print('You work for a pizza parlor and are responsible for making and delivering the pizzas on time.')
+print('You have 30 minutes to make the pizza and 30 minutes to deliver the pizza.')
+
+start = input('Type START to start')
+print(number)
+
