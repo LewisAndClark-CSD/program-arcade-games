6ea7b625ee2ce0eaaef640a8ad540046aac0619c
EvanWitterholt <33132584+EvanWitterholt@users.noreply.github.com>
2017-11-10 13:45:30 -0600

    Update main_program.py

--- Lab 05 - Create a Picture/main_program.py
+++ Lab 05 - Create a Picture/main_program.py
@@ -1,1 +1,37 @@
-#
+#!/usr/bin/env python3
+# Drawing a picture in python
+# Evan Witterholt
+# 11/11/17
+
+import pygame
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+ 
+size = (700, 500)
+screen = pygame.display.set_mode(size)
+
+done = False
+
+clock = pygame.time.Clock()
+
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+    screen.fill(GREEN)
+
+    pygame.draw.rect(screen,BLACK,[-25,35,750,150],2)
+
+    pygame.display.flip()
+
+    clock.tick(60)
+
+  
+pygame.quit()
+
+
+
