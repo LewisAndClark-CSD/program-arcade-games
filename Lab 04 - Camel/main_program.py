c0fb86471337e7ec245d05c3eef34ccf2cdb9b85
Conner Walkenhorst <33132628+ConnerW1719@users.noreply.github.com>
2017-11-10 10:17:56 -0600

    Update main_program.py

--- Lab 04 - Camel/main_program.py
+++ Lab 04 - Camel/main_program.py
@@ -1,1 +1,9 @@
 #
+# Lab 04 Camel
+# Conner Walkenhorst
+# 11/10/2017
+
+"""the user is running from a hoard of zombies, the user decides what to do"""
+
+print('Welcome to Zombie Run!')
+print('You have found yourself being chased by a hoard of flesh eating zombies while going on a supply run. The hoard is coming for you fast. Survive at all costs and outrun the hoard.')
