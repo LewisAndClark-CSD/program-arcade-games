8364565820175df5bb5c88902b06e20728a466eb
EthanEdDall <edall@stcharlessd.org>
2017-11-14 13:39:26 -0600

    problems 1 - 6 worksheet6

---
+++ Lab 06 - Loopy Lab/6.2 Advanced Looping Problems pt12.py
@@ -0,0 +1,38 @@
+#!/user/bin/env python3
+#print 10 *
+#Ethan Dall
+#11-13-17
+
+"""Print numbers counting up then down in rows"""
+
+#Do 9 times starting at 1
+for y in range(1, 10):
+    
+    #loop to print spaces
+    for x in range(9 - y):
+        print(' ', end=' ')
+        
+    #loop to print digits
+    for x in range(y):
+        print(x+1, end=' ')
+    
+    #loop to print digits2
+    for x in range(y, 1, -1):
+        print(x-1, end=' ')
+    
+    #print a new line
+    print()
+
+#prints 8 times from 8
+for y in range(9, 1, -1):
+    
+    #loop to print spaces
+    for x in range(10-y):
+        print(' ', end=' ')
+    
+    #loop prints digits
+    for x in range(1, y):
+        print(x, end=' ')
+        
+    #print a new line
+    print()
--- Lab 06 - Loopy Lab/part_2.py
+++ Lab 06 - Loopy Lab/part_2.py
@@ -1,1 +1,19 @@
-#
+#!/user/bin/env python3
+#part_2.py
+#Ethan Dall
+#11-14-17
+
+"""User input determines height of box"""
+
+#Ask Question
+height = int(input('How tall do you want the box to be? '))
+
+#Top line
+print('o'*10)
+
+#colums
+for x in range(height):
+    print('o', ' '*6, 'o', end='\n')
+        
+#Bottom line
+print('o'*10)
