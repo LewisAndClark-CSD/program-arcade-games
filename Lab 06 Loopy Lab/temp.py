a47b901047d89ca24375bb0c30158a2ee4785ce6
BradyBallmann <bballmann@stcharlessd.org>
2017-11-14 11:57:58 -0600

    Made * table

--- Lab 06 Loopy Lab/temp.py
+++ Lab 06 Loopy Lab/temp.py
@@ -1,1 +1,13 @@
-#
+#!/usr/bin/env python3
+# Loops
+# Brady Ballmann
+# 11/13/2017
+
+# Do 9 times starting at 1
+for row in range(1, 10):
+    # Loop to print multiples 
+    for i in range(1, 10):
+        print(row * i, end=" ")
+    # Print new line    
+    print()
+    
