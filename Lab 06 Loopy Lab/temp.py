b7992a86aa3827050f43231c8fe89f4e0221d6b1
BradyBallmann <bballmann@stcharlessd.org>
2017-11-14 12:24:06 -0600

    number triangle

--- Lab 06 Loopy Lab/temp.py
+++ Lab 06 Loopy Lab/temp.py
@@ -5,9 +5,15 @@
 
 # Do 9 times starting at 1
 for row in range(1, 10):
-    # Loop to print multiples 
-    for i in range(1, 10):
-        print(row * i, end=" ")
-    # Print new line    
+    # Loop to print spaces
+    for space in range(9 - row):
+        print(" ", end=" ")
+    # Loop to print digits 
+    for digits in range(1, row + 1):
+        print(digits, end=" ")
+    # Loop to print digits2
+    for digits2 in range(row -1, 0, -1):
+        print(digits2, end=" ")
+    # Print a new line
     print()
     
