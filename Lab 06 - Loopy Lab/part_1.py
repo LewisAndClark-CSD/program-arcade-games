e305b30dcd6197738acb8b9cae9cde965cf5f25f
Jrhod99 <jarhodes@stcharlessd.org>
2017-11-15 08:12:58 -0600

    Finished Chp.6 part 1 Lab

--- Lab 06 - Loopy Lab/part_1.py
+++ Lab 06 - Loopy Lab/part_1.py
@@ -1,1 +1,14 @@
+#!/usr/bin/env python3
+#part_1.py
+#Jared Rhodes
+#November 5, 2017
 
+""" Part 1 of Chapter 6 Loopy Lab """
+
+diget = 10
+
+for row in range(1,10):
+    for col in range(row):
+        print(diget,end=" ")
+        diget += 1
+    print()
