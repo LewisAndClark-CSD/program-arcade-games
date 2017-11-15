c56bf5ff6b03958997905aed8f7022debb50f07f
ChristianGroves <cgroves@stcharlessd.org>
2017-11-15 08:07:53 -0600

    Completed part1 of lab6

--- Lab 06 - Loopy Lab/part_1.py
+++ Lab 06 - Loopy Lab/part_1.py
@@ -1,1 +1,12 @@
+#!/usr/bin/env python3
+#Lab #6
+#Christian Groves
+#11-15-17
 
+#for loop to add 
+num = 10
+for row in range(1,10):
+    for col in range(row,0,-1):
+        print(num,end=' ')
+        num += 1
+    print()
