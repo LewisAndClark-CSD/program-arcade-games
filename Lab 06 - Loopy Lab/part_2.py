5a11682141bfd8ce963348f6469482738272bd45
BradyBallmann <bballmann@stcharlessd.org>
2017-11-15 12:14:33 -0600

    Made the top row for the boxes

--- Lab 06 - Loopy Lab/part_2.py
+++ Lab 06 - Loopy Lab/part_2.py
@@ -1,1 +1,15 @@
-#
+#!/usr/bin/env python3
+# part_2 of chapter 6 Lab
+# Brady Ballmann
+# 11/15/2017
+
+# Input statement for the o's
+numberos = int(input("How many o's do you want? "))
+ 
+number = numberos * 2
+# Loop for the top of the box
+for row in range(number):
+    print("o",end="")
+    # 
+
+
