be5bb64c559f52bf0ea2d1f1257414275151bcd9
jacob poncher <jponcher@stcharlessd.org>
2017-11-15 12:29:28 -0600

    part 1 first try

--- Lab 06 - Loopy Lab/part_1.py
+++ Lab 06 - Loopy Lab/part_1.py
@@ -1,1 +1,18 @@
+# !/usr/bin/env/ python3
+#part 1 
+#jacob poncher
+#11/15/17
 
+# start a variable at 10
+x = 10 
+
+#create a loop to do 9 rows
+for i in range(1,10):
+        #create a loop to print digits
+        for j in range(i):
+                #print the variable
+                print(x, end=' ')
+                #increment the variable
+                x= x+1
+                #print a new line
+                print()
