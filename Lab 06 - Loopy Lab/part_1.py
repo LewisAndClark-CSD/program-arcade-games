b94e88e8036e4287a15cfd007a93aae4da166c4e
ryamoore <rymoore@stcharlessd.org>
2017-11-15 12:10:13 -0600

    got bradys help because was over thinking it

--- Lab 06 - Loopy Lab/part_1.py
+++ Lab 06 - Loopy Lab/part_1.py
@@ -2,11 +2,12 @@
 # part_1.py
 # Ryan Moore
 # 11/15/2017
-
+start = 10
 #loop 9 times 
-for row in range(10):
-     for column in range(10,55):
-          print(column,end=" ")
-#increase by one,two,three etc each time you go do           
-      #print new line
+for row in range(9):
+#increase by one,two,three etc each time you go do     
+     for column in range(row+1):
+          print(start ,end=" ")
+          start += 1            
+     #print new line
      print()
