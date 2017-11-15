f851b1dac0fef44b4ec53122f7f5f779d686315e
JasonC07 <jcobb@stcharlessd.org>
2017-11-15 08:53:32 -0600

    first attempt

--- Lab 06 - Loopy Lab/part_1.py
+++ Lab 06 - Loopy Lab/part_1.py
@@ -1,1 +1,15 @@
+#Set a variable to ten
+counter = 10
 
+#Repeat nine times
+for i in range(9):
+    
+    
+    #Loop to print numbers in a line
+    for j in range(i + 1):
+        print(counter, end=" ")
+        #increatment the number
+        counter += 1
+        #print new line
+        print()
+        
