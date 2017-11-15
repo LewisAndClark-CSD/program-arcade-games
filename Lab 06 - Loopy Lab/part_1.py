588444b5a00b39f396e4dc100a33da88ce8a4356
LaurenBoehme <LBOEHME@stcharlessd.org>
2017-11-15 10:11:22 -0600

    Completed Chapter 6

--- Lab 06 - Loopy Lab/part_1.py
+++ Lab 06 - Loopy Lab/part_1.py
@@ -1,1 +1,12 @@
+# !/usr/bin/env python3
+#Lab 6 part one
+#Lauren Boehme
+#11/15/17
 
+k=10
+for i in range(9):
+    for j in range(i+1):
+        print(k, end=' ')
+        k+=1
+    print(' ', end=' ')
+    print()
--- Lab 06 - Loopy Lab/part_3.py
+++ Lab 06 - Loopy Lab/part_3.py
@@ -1,1 +1,27 @@
-#
+# !/usr/bin/env python3
+#Lab 6 part three
+#Lauren Boehme
+#11/15/17
+inValue = int(input('Input integer:'))
+
+base = ' '
+for i in range(inValue):
+    base = ' '+str(2*(inValue-1-i)+1)+base+str(2*(inValue-1-i)+1)+' '
+
+length = len(base.strip())
+graph = []
+for j in range(inValue):
+    line = ' '
+    for k in range(j):
+        line = '  ' + line + '  '
+    for i in range(inValue-j):
+        line = ' '+str(2*(inValue-1-i)+1)+line+str(2*(inValue-1-i)+1)+' '
+    line = line.strip()
+    length_temp = int((length-len(line))/2)
+    for m in range(length_temp):
+        line = ' ' + line + ' '
+    graph.append(line)
+for j in range(inValue) :
+    graph.append(graph[inValue-1-j])
+for line in graph:
+    print(line)
