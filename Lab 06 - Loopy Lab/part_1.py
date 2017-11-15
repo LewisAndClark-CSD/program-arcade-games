8dfaa6a850494516f8c087b3b842130f7d9b7b05
JeLyles <jlyles@stcharlessd.org>
2017-11-15 12:29:24 -0600

    Got the numbers into the triangle form.

--- Lab 06 - Loopy Lab/part_1.py
+++ Lab 06 - Loopy Lab/part_1.py
@@ -1,1 +1,12 @@
+#!/usr/bin/env python3
+# Chapter 6 Lab Part 1
+# Jessica Lyles
+# 11/15/2017
 
+# Do 9 times starting at 10 (row)
+for i in range(9):
+    # Loop to print the digits
+    for j in range(i + 1):
+        print(j, end=' ')
+    #print new line
+    print()
