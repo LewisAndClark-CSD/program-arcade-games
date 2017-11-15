b479d55e8e7d72b9f9e04b8b3745d3c4f1959dcc
JeLyles <jlyles@stcharlessd.org>
2017-11-15 13:36:01 -0600

    Set up a loop to print an amount of o's times two dependng on input

--- Lab 06 - Loopy Lab/part_2.py
+++ Lab 06 - Loopy Lab/part_2.py
@@ -3,4 +3,9 @@
 # Jessica Lyles
 # 11/15/2017
 
+# User Input for value of n/how many o's (row wise not column)
 userInput = int(input('Please enter a value for n: '))
+# Do as many times as the user specifies
+for i in range(userInput):
+    print('o'*2, end = '')
+
