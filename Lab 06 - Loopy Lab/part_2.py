94e9171a44794be1f0364d85cdbc2bd3ade2c7ff
JasonC07 <jcobb@stcharlessd.org>
2017-11-15 09:22:04 -0600

    Forgot to do a commit but Completed Chapter 6 Lab  part 2

--- Lab 06 - Loopy Lab/part_2.py
+++ Lab 06 - Loopy Lab/part_2.py
@@ -1,1 +1,17 @@
-#
+#Do an input statement to make box any size user wants
+n = int(input("Desired size: "))
+
+#Variable for spaces
+spaces = (n*2) - 2
+
+#print the o times the variable and times 2
+print('o' * ((n)*2))
+
+#loop variable - 1
+for i in range(1, n-1):
+    
+#print the o's + spaces times and then plus another o    
+    print('o' + (' '*spaces) + 'o')
+    
+#print the final row of o's times the variable and 2.    
+print('o'*(n)*2)
