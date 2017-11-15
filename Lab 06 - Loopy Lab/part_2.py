df7a0ece73e53303e163ab2bf533ffc9019bfb77
Sthomp123 <sthompson@stcharlessd.org>
2017-11-15 12:25:45 -0600

    Made loop that draws the top line

--- Lab 06 - Loopy Lab/part_2.py
+++ Lab 06 - Loopy Lab/part_2.py
@@ -1,1 +1,13 @@
-#
+#!/usr/bin/env python3
+# part_2.py
+# Steven Thompson
+# November 15th 2017
+
+""" This program makes a boxes of 'o's based on user input """
+
+
+n = int(input("Enter a number: "))
+for i in range(n*2):
+    print("o", end = "")
+print()
+    
