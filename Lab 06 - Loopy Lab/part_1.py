cfc24717a5650e6868652621f41e53e6eb7d1aa1
JeLyles <jlyles@stcharlessd.org>
2017-11-15 13:16:45 -0600

    Set and used 'start' variable to print the right numbers.

--- Lab 06 - Loopy Lab/part_1.py
+++ Lab 06 - Loopy Lab/part_1.py
@@ -3,10 +3,13 @@
 # Jessica Lyles
 # 11/15/2017
 
+# Variable for start
+start = 10
 # Do 9 times starting at 10 (row)
 for i in range(9):
     # Loop to print the digits
     for j in range(i + 1):
-        print(j, end=' ')
+        print(start, end=' ')
+        start += 1
     #print new line
     print()
