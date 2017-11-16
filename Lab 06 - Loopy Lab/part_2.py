0489e6daae240643a85ac22823f7e1d40fcd7b08
jriebow <jriebow@stcharlessd.org>
2017-11-16 11:42:02 -0600

    Completed Chapter 6 Part 2

--- Lab 06 - Loopy Lab/part_2.py
+++ Lab 06 - Loopy Lab/part_2.py
@@ -1,1 +1,20 @@
-#
+#!/usr/bin/env python3
+#part_2.py
+#Justin Riebow
+#11/16/17
+
+"""Creates a box made of o's"""
+
+#Make an input for the number of o's in a row
+row = int(input("Enter number: "))
+#Draws top line
+for toprow in range(row*2):
+    print("o", end="")
+print()
+#Draws side
+for side in range(row-2):
+    print("o", (row-2) * "  ", "o")
+#Draws bottom line
+for bottomrow in range(row*2):
+    print("o", end="")
+print ()
