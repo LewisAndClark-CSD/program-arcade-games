c9ba45c7714a901a85cac9425b9b561b50db74cb
Jake Faucett <jfaucett@PC02.CIS.COM>
2017-11-16 13:00:02 -0600

    CHapter 6 completed

--- Lab 03 - Create a Quiz/main_program.py
+++ Lab 03 - Create a Quiz/main_program.py
@@ -1,1 +1,9 @@
-#
+#!/user/bin/env python3
+#chap6.py
+#Jake Faucett
+#11/13/17
+
+print('Quiz Time!')
+
+input('What side of my head do I have a implant on? ')
+if "Left"
--- Lab 06 - Loopy Lab/part_1.py
+++ Lab 06 - Loopy Lab/part_1.py
@@ -1,1 +1,17 @@
+#!/user/bin/env python3
+#loopy lab
+#Jake Faucett
+#11/16/17
 
+#Start at 10 for the triangle
+begin = 10
+#Do this 9 times
+for row in range(9):
+    # Loop to print the digits
+    for j in range(row +1):
+        #prints the variable
+        print(begin, end= ' ')
+        #Adds 1 to start so the line is higher
+        begin += 1
+    #Prints a new line 
+    print()
