23f89125467a7c602d68447327014fd706ca0092
jriebow <jriebow@stcharlessd.org>
2017-11-16 11:22:04 -0600

    Completed Chapter 6 Part 1

--- Lab 06 - Loopy Lab/part_1.py
+++ Lab 06 - Loopy Lab/part_1.py
@@ -1,1 +1,13 @@
+#!/usr/bin/env python3
+#part_1.py
+#Justin Riebow
+#11/15/17
 
+"""Prints out numbers in a certain order"""
+
+i = 10
+for row in range(1, 10):
+    for column in range(row):
+        print(i,end=" ")
+        i += 1
+    print()
