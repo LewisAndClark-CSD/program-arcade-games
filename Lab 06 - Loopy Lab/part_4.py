53c4a5bf0a85a325f60cdc546721ed88b21bc142
ryamoore <rymoore@stcharlessd.org>
2017-11-16 11:43:05 -0600

    got the grid to show removed row and column

--- Lab 06 - Loopy Lab/part_4.py
+++ Lab 06 - Loopy Lab/part_4.py
@@ -11,6 +11,7 @@ WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 BLUE= (0, 0, 255)
+PI = 3.141592653
  
 pygame.init()
  
@@ -21,34 +22,24 @@ screen = pygame.display.set_mode(size)
 pygame.display.set_caption("My Game")
  
 # Loop until the user clicks the close button.
-done = False
- 
-# Used to manage how fast the screen updates
+done = False 
 clock = pygame.time.Clock()
  
-# -------- Main Program Loop -----------
 while not done:
-    # --- Main event loop
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            done = True
- 
-    # --- Game logic should go here
- 
-    # --- Screen-clearing code goes here
- 
-    # Here, we clear the screen to white. Don't put other drawing commands
-    # above this, or they will be erased with this command.
- 
-    # If you want a background image, replace this clear with blit'ing the
+            done = True    
     # background image.
     screen.fill(BLACK)
  
-    # --- Drawing code should go here
- 
     # --- Go ahead and update the screen with what we've drawn.
+    for y in range(0, 500, 8):
+        for x in range(0, 700, 8):
+            pygame.draw.rect(screen,GREEN,[x, y, 4, 4],0)
+        print()
+                    
     pygame.display.flip()
- 
     # --- Limit to 60 frames per second
     clock.tick(60)
  
