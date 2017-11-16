db1d133e56259eb145d76a3cc85773f9c4583ac1
ChristianGroves <cgroves@stcharlessd.org>
2017-11-16 10:08:03 -0600

    completed ch5 ws and started lab by creating the outline and grass for a canvas

--- Lab 05 - Create a Picture/main_program.py
+++ Lab 05 - Create a Picture/main_program.py
@@ -1,1 +1,35 @@
-#
+#!/usr/bin/env python3
+#Lab #5
+#Christian Groves
+#11/16/17
+import pygame
+pygame.init()
+BLACK   =(0,0,0)
+WHITE   =(255,255,255)
+GREEN   =(0,255,0)
+RED     =(255,0,0)
+BLUE    =(0,0,255)
+size=(700,500)
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Christian's Cool Game")
+done = False
+clock = pygame.time.Clock()
+font = pygame.font.Font(None, 25)
+while done == False:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done=True
+            
+            
+    screen.fill(BLUE)        
+    
+    pygame.draw.line(screen, GREEN , [0,400], [700,400],200)        
+    pygame.draw.ellipse(screen, WHITE, [20, 20, 250, 100], 50)
+    
+    
+         
+    pygame.display.flip()        
+             
+    clock.tick(20)
+    
+pygame.quit  ()
--- Lab 06 - Loopy Lab/part_3.py
+++ Lab 06 - Loopy Lab/part_3.py
@@ -1,1 +1,23 @@
-#
+#!/usr/bin/env python3
+#Lab #6
+#Christian Groves
+#11-15-17
+size = int(input('Size: '))
+
+
+for row in range(1,10):
+    for digit1 in range(9 - row):
+        print(digit1, end=' ')
+    for spaces in range(1,row +1):
+        print(' ',end=' ')
+    for digit2 in range(row,0,-1):
+        print(digit2,end=' ')    
+    print()
+for botrow in range (9,0,-1):
+    for spacesbot in range(9- botrow, 0, -1):
+        print(' ',end=' ')
+    for digit3 in range(1, botrow+1):
+        print(digit3,end=' ')
+    for digit4 in range(botrow,0,-1):
+        print(digit4,end=' ')      
+    print()
