6bd091630183cebfa41bac75fec71e4891f7121f
JeLyles <jlyles@stcharlessd.org>
2017-11-16 11:30:13 -0600

    Got the ex. n = 3 working, need to figure how to do it based on userInput.

--- Lab 06 - Loopy Lab/part_2.py
+++ Lab 06 - Loopy Lab/part_2.py
@@ -5,12 +5,19 @@
 
 # User Input for value of n/how many o's (row wise not column)
 userInput = int(input('Please enter a value for n: '))
-# Do as many times as the user specifies
+# Loop to print top line of o's
 for i in range(userInput):
     print('o'*2, end = '')
+# Print new line
 print()
+# Loop to print middle o's row
+for row in range(2):
+    print('o', ' '*userInput, end = '')
+print()
+# Loop to print bottom line of o's
 for j in range(userInput):
     print('o'*2, end = '')
+
     
     
 
