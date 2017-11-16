f1fa859bf5e34a22c421f543ee581f67c90b6551
Sthomp123 <sthompson@stcharlessd.org>
2017-11-16 11:21:29 -0600

    Added comments

--- Lab 06 - Loopy Lab/part_2.py
+++ Lab 06 - Loopy Lab/part_2.py
@@ -7,14 +7,16 @@
 
 
 n = int(input("Enter a number: "))
+
+#This loop makes the top line of o's
 for i in range(n*2):
     print("o", end= "")
 print()
-
+#This loop makes the spaces and vertical o's
 for k in range(1,n-1):
     print("o", (n-2)*"  ", "o")
 
-        
+#This loop makes the final line of o's        
 for j in range(n*2):
         print("o", end= "")
 
