e0667f0d1420f21f40467c65863fe1f05fee769a
JeLyles <jlyles@stcharlessd.org>
2017-11-16 12:05:26 -0600

    Fixed loop and added variable to make program work for all userInputs. Finished part 2.

--- Lab 06 - Loopy Lab/part_2.py
+++ Lab 06 - Loopy Lab/part_2.py
@@ -5,15 +5,16 @@
 
 # User Input for value of n/how many o's (row wise not column)
 userInput = int(input('Please enter a value for n: '))
+# Variable for spaces between
+spaces = (userInput*2) - 2
 # Loop to print top line of o's
 for i in range(userInput):
     print('o'*2, end = '')
 # Print new line
 print()
-# Loop to print middle o's row
-for row in range(2):
-    print('o', ' '*userInput, end = '')
-print()
+# Loop to print o's between
+for row in range(1, userInput - 1):
+    print('o' +  (' ' * spaces) + 'o')
 # Loop to print bottom line of o's
 for j in range(userInput):
     print('o'*2, end = '')
