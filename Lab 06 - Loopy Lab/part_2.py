0cf8e2dd199e680dde59771e48547f245f548901
EvieNelson <mnelson@stcharlessd.org>
2017-11-16 08:38:17 -0600

    Completed lab 2 of chapter 6

--- Lab 06 - Loopy Lab/part_2.py
+++ Lab 06 - Loopy Lab/part_2.py
@@ -1,11 +1,13 @@
 #Chapter 6 Lab 2
 number = int(input("Number = "))
 letter = "o"
+spaces = " " * ((number * 2) - 3)
 for row in range(number):
     print(letter*2, end="")
 print()
 for row in range(number-2):
-    print(letter)              
+    print(letter, spaces, end="")
+    print(letter)
 for row in range(number):
     print(letter*2, end="")
 print()
