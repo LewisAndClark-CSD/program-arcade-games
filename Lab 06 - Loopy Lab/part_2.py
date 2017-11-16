0263677538a0fd7e4dd721e2b297071c54adddbb
EvieNelson <mnelson@stcharlessd.org>
2017-11-16 08:23:47 -0600

    Modified chapter 6 lab 2, not complete

--- Lab 06 - Loopy Lab/part_2.py
+++ Lab 06 - Loopy Lab/part_2.py
@@ -1,1 +1,11 @@
-#
+#Chapter 6 Lab 2
+number = int(input("Number = "))
+letter = "o"
+for row in range(number):
+    print(letter*2, end="")
+print()
+for row in range(number-2):
+    print(letter)              
+for row in range(number):
+    print(letter*2, end="")
+print()
