a1e68f91711dfc353376576faa330fc5b1b557d4
ChristianGroves <cgroves@stcharlessd.org>
2017-11-17 08:05:14 -0600

    added some clouds

--- Lab 05 - Create a Picture/main_program.py
+++ Lab 05 - Create a Picture/main_program.py
@@ -9,6 +9,7 @@ WHITE   =(255,255,255)
 GREEN   =(0,255,0)
 RED     =(255,0,0)
 BLUE    =(0,0,255)
+PURPLE  =(150,20,130)
 size=(700,500)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Christian's Cool Game")
@@ -22,9 +23,21 @@ while done == False:
             
             
     screen.fill(BLUE)        
+    #green ground
+    pygame.draw.line(screen, GREEN , [0,400], [700,400],200) 
+    #first cloud
+    pygame.draw.ellipse(screen, WHITE, [40, 20, 130, 90])
+    pygame.draw.ellipse(screen, WHITE, [70, 30, 140, 110])
+    pygame.draw.ellipse(screen, WHITE, [20, 40, 140, 110])
+    #second cloud
+    pygame.draw.ellipse(screen, WHITE, [440, 50, 140, 110])
+    pygame.draw.ellipse(screen, WHITE, [460, 20, 140, 110])
+    pygame.draw.ellipse(screen, WHITE, [480, 60, 140, 110])
+    #House
+    pygame.draw.rect(screen, PURPLE, [300, 320, 300, 320])
     
-    pygame.draw.line(screen, GREEN , [0,400], [700,400],200)        
-    pygame.draw.ellipse(screen, WHITE, [20, 20, 250, 100], 50)
+    
+    
     
     
          
