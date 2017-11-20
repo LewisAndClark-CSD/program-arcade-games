aa4e104e05a762975a5fa270b8e1370e0717ec31
ryamoore <rymoore@stcharlessd.org>
2017-11-20 12:33:19 -0600

    add almost all the room's and put them in a list

--- Lab 07 - Adventure/main_program.py
+++ Lab 07 - Adventure/main_program.py
@@ -1,1 +1,20 @@
-#
+#!/usr/bin/env python3
+# main_program.py
+# Ryan Moore
+# 11/20/2017
+
+""" runs a map then the game """
+
+room_list[]
+room = ["You have just awoken in a jail cell of a ship",0,None,None,None]
+room_list.append(room)
+room = ["You are in a big hallway",1,None,None,None]
+room_list.append(room)
+room = [" You are in the hang out area with only a few people",4,None,None,None]
+room_list.append(room)
+room = ["You are in a hallway and can kinda see what is on the other side",None,None,None,3]
+room_list.append(room)
+room = ["You are in the guard's room but the people are asleep",None,None,None,2]
+room_list.append(room)
+room = ["You are in a hallway and smell something getting stronger",None,5,None,None]
+room_list.append(room)
