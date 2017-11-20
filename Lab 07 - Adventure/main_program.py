ae9e9d498841a02fed861ffaad4a8d0d08b952fd
JasonC07 <jcobb@stcharlessd.org>
2017-11-20 09:31:18 -0600

    Tried to run program didn't go well

--- Lab 07 - Adventure/main_program.py
+++ Lab 07 - Adventure/main_program.py
@@ -2,13 +2,19 @@
 room_list = []
 
 #Create a variable called room with an array of five elements.
-room = [4]
+room = ["North hall", "Dining room", "South hall", "Bedroom 1", "Bedroom 2", "Balcony"]
 #First element create a string with description of the first room.
-
+First = "North hall" + " Pictures of angels fighting demons line the walls."  
 #Other four will be number of the next room if user goes north, south, east, or west.
-
+north = 0 
+south = 1
+east = 2
+west = 3
 #Make sure to write which number connects to which other.
-
+if user goes west from North hall they will be in the kitchen. 
+if user goes east from North hall they will be in the Bedroom 1.
+if user goes south from North hall they will be in the Bedroom 2.
+if user goes north from the North hall they will be None.
 #If there isn't one at a location like none at the west then write None but NOT in quotes.
 
 #Append the room to the room list
