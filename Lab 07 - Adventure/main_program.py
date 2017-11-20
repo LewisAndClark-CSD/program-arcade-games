cfbffd8f9b83df05b5c0b727388c3620891b7035
JeLyles <jlyles@stcharlessd.org>
2017-11-20 11:41:20 -0600

    Set up room variables.

--- Lab 07 - Adventure/main_program.py
+++ Lab 07 - Adventure/main_program.py
@@ -1,1 +1,17 @@
-#
+#Chapter 7 Lab
+
+room_list = []
+room = ["Entrance", None, None, 2, None]
+room_list.append(room)
+room = ["Kid's Room", None, 2, 4, None]
+room_list.append(room)
+room = ["Hallway", 0, 3, 5, 1]
+room_list.append(room)
+room = ["Bathroom", None, None, None, 2]
+room_list.append(room)
+room = ["Kid's Closet", 1, None, None, None]
+room_list.append(room)
+room = ["Parent's Room", 2, None, 6, None]
+room_list.append(room)
+room = ["Parent's Bathroom", 5, None, None, None]
+room_list.append(room)
