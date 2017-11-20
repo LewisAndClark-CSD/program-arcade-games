73de6f5517243b991d3a4e867e7ed3bdddf5062e
JeLyles <jlyles@stcharlessd.org>
2017-11-20 13:32:18 -0600

    Added /n to seperate lines. Finished Lab 7.

--- Lab 07 - Adventure/main_program.py
+++ Lab 07 - Adventure/main_program.py
@@ -1,25 +1,25 @@
 #Chapter 7 Lab
 
 room_list = []
-room = ["You are standing in the entrance of the Mystery House. To the south is the rest of the rooms.", None, None, 2, None]
+room = ["You are standing in the entrance of the Mystery House.\nTo the south is the rest of the rooms.", None, None, 2, None]
 room_list.append(room)
 
-room = ["You are standing in the Mystery Children's Room. To the south and east are the other rooms.", None, 2, 4, None]
+room = ["You are standing in the Mystery Children's Room.\nTo the south and east are the other rooms.", None, 2, 4, None]
 room_list.append(room)
 
-room = ["You are standing in the hallway of the Mystery house. There are rooms in all directions.", 0, 3, 5, 1]
+room = ["You are standing in the hallway of the Mystery house.\nThere are rooms in all directions.", 0, 3, 5, 1]
 room_list.append(room)
 
-room = ["You are standing in the Mystery Bathroom. The rest of the Mystery House is to the west.", None, None, None, 2]
+room = ["You are standing in the Mystery Bathroom.\nThe rest of the Mystery House is to the west.", None, None, None, 2]
 room_list.append(room)
 
-room = ["You are standing in the Mystery Children's Closet. There is a room to the north.", 1, None, None, None]
+room = ["You are standing in the Mystery Children's Closet.\nThere is a room to the north.", 1, None, None, None]
 room_list.append(room)
 
-room = ["You are standing in the Mystery Parent's Room. Two rooms lay to the north and south.", 2, None, 6, None]
+room = ["You are standing in the Mystery Parent's Room.\nTwo rooms lay to the north and south.", 2, None, 6, None]
 room_list.append(room)
 
-room = ["You are in the Mystery Parent's Bathroom. One room is to the north.", 5, None, None, None]
+room = ["You are in the Mystery Parent's Bathroom.\nOne room is to the north.", 5, None, None, None]
 room_list.append(room)
 
 current_room = 0
