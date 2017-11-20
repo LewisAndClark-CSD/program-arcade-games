e7233dbcd5ea03d27e9fda6ee64503d22536da6e
JasonC07 <jcobb@stcharlessd.org>
2017-11-20 10:03:44 -0600

    Finished room variable I believe.

--- Lab 07 - Adventure/main_program.py
+++ Lab 07 - Adventure/main_program.py
@@ -3,6 +3,7 @@ room_list = []
 
 #Create a variable called room with an array of five elements.
 room = ["North hall", None, 4, 5, 1]
+#Append the room to the room list
 room_list.append(room)
 room = ["Kitchen", None, 0, 2, None]
 room_list.append(room)
@@ -17,26 +18,11 @@ room_list.append(room)
 room = ["Balcony", 3, None, None, None]
 room_list.append(room)
 #First element create a string with description of the first room.
-First = "North hall" + " Pictures of angels fighting demons line the walls."  
-#Other four will be number of the next room if user goes north, south, east, or west.
-north = 0 
-south = 1
-east = 2
-west = 3
-#Make sure to write which number connects to which other.
-if user goes west from North hall they will be in the kitchen. 
-if user goes east from North hall they will be in the Bedroom 1.
-if user goes south from North hall they will be in the Bedroom 2.
-if user goes north from the North hall they will be None.
-#If there isn't one at a location like none at the west then write None but NOT in quotes.
-
-#Append the room to the room list
+First = "North hall" + " Pictures of angels fighting demons line the walls."
 
 #Repeat the prior two steps for each foom you want to create. 
-
 #Re-use the room variable
-
 #Make new variable called current_room set it to zero.
 current_room = 0
 #print the room_list variable run the program to test it.
-print(room_list)
+print(room_list[0])
