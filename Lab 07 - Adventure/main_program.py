1c53fc0269a0eb2fa80390ad6ffa036b92872687
EvieNelson <mnelson@stcharlessd.org>
2017-11-28 07:33:15 -0600

    Modified worksheet 5 and lab 7

--- Lab 07 - Adventure/main_program.py
+++ Lab 07 - Adventure/main_program.py
@@ -1,1 +1,15 @@
-#
+#Lab 7: Adventure
+#First, sketch the layout of the dungeon
+roomList = [roomZero, roomOne, roomTwo, roomThree, roomFour, roomFive, roomSix, roomSeven, roomEight]
+#All of the rooms:
+roomZero = ["Main hall", 3, 2, None, 1]
+roomOne = ["Ballroom", None, 0, None, None]
+roomTwo = ["Study", 4, None, None, 0]
+roomThree = ["West Hall", None, 4, 0, 5]
+roomFour = ["East Hall", 6, 5, 2, 3]
+roomFive = ["Weapon Room", None, None, None, 4]
+roomSix = ["Meeting Room", None, None, 4, None]
+roomSeven = ["Bedroom", 8, 3, None, None]
+roomEight = ["Secret Room", None, None, 7, None]
+currentRoom = 0
+print(roomList)
