06a304543af00678fd3d035aead63cdfb823f35f
ConnerW1719 <cwalkenhorst@stcharlessd.org>
2017-11-28 11:30:32 -0600

    test 1 for lab 7 text adventure.

--- Lab 07 - Adventure/main_program.py
+++ Lab 07 - Adventure/main_program.py
@@ -5,3 +5,22 @@
 
 """Create a text adventure game using python"""
 
+room_list = []
+room = ["The Entryway", None, None, 3, None]
+room_list.append(room)
+room = ["Office Room", None, None, 4, None]
+room_list.append(room)
+room = ["Ballpit Room", None, None, 5, None]
+room_list.append(room)
+room = ["West Hall", 0, 4, 6, None]
+room_list.append(room)
+room = ["Center Hall", 1, 5, 7, 3]
+room_list.append(room)
+room = ["East Hall", 2, None, 8, 4]
+room_list.append(room)
+room = ["Weight Room", 2, None, None, None]
+room_list.append(room)
+room = ["Knive Room", 4, None, None, None]
+room_list.append(room)
+room = ["Balloon Room", 5, None, None, None]
+room_list.append(room)
