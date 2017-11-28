970a34468b9d8021c69c1ba8ba48d86f4beb5147
ChristianGroves <cgroves@stcharlessd.org>
2017-11-28 07:28:40 -0600

    finished:   Lab 05 - Create a Picture/main_program.py 	finished:   Lab 06 - Loopy Lab/part_4.py 	modified:   Lab 07 - Adventure/main_program.py

--- Lab 05 - Create a Picture/main_program.py
+++ Lab 05 - Create a Picture/main_program.py
@@ -4,6 +4,10 @@
 #11/16/17
 import pygame
 pygame.init()
+greenColor = 175
+radius = 180
+sunChange= (200,greenColor,40)
+LIGHTBLUE=(126,213,242)
 BLACK   =(0,0,0)
 WHITE   =(255,255,255)
 GREEN   =(0,255,0)
@@ -11,17 +15,13 @@ RED     =(255,0,0)
 BLUE    =(0,0,255)
 PURPLE  =(150,20,130)
 size=(700,500)
+xoff= 700
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Christian's Cool Game")
 done = False
 clock = pygame.time.Clock()
 font = pygame.font.Font(None, 25)
 while done == False:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done=True
-            
-            
     screen.fill(BLUE)        
     #green ground
     pygame.draw.line(screen, GREEN , [0,400], [700,400],200) 
@@ -32,13 +32,17 @@ while done == False:
     #second cloud
     pygame.draw.ellipse(screen, WHITE, [440, 50, 140, 110])
     pygame.draw.ellipse(screen, WHITE, [460, 20, 140, 110])
-    pygame.draw.ellipse(screen, WHITE, [480, 60, 140, 110])
-    #House
-    pygame.draw.rect(screen, PURPLE, [300, 320, 300, 320])
-    
-    
-    
-    
+    pygame.draw.ellipse(screen, WHITE, [480, 60, 140, 110])    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done=True
+
+    #House's
+    for xoff in range(700, 0, -200):
+        pygame.draw.rect(screen, PURPLE, [50+ xoff , 260, 100, 110])
+        pygame.draw.rect(screen, BLACK, [70+ xoff , 300, 30, 70])
+        pygame.draw.rect(screen, WHITE, [110+ xoff , 300, 30, 30])
+        pygame.draw.polygon(screen, RED, [[25+ xoff , 260], [175+ xoff, 260], [100+ xoff , 200]]) 
     
          
     pygame.display.flip()        
--- Lab 07 - Adventure/main_program.py
+++ Lab 07 - Adventure/main_program.py
@@ -1,1 +1,46 @@
-#
+#!/usr/bin/env python3
+#Lab 6
+#Christian Groves
+#11/20/17
+roomList = []
+#CMN
+room = ['You are in Computer Maintinance Networking. \nCentral hallway to the North.', 3, None, None, None]
+roomList.append(room)
+#CSD
+room = ['You are in Computer Software Development. \nEast hallway to the North.', 4, None, None, None]
+roomList.append(room)
+#HALL0
+room = ['You are in the West Hallway. \nBathroom is to the North. Central hallway is to the East.', 5, 3, None, None]
+roomList.append(room)
+#HALL1
+room = ['You are in the Central Hallway. \nWelding is to the North. East hallway is to the East. \nCMN is to the South. West hallway is to the West.', 6, 4, 0, 2]
+roomList.append(room)
+#HALL2
+room = ['You are in the East Hallway. \nAuto Collision is to the North. CSD is to the South. \nCentral hallway is to the West.', 7, None, 1, 3]
+roomList.append(room)
+#BATHROOM
+room = ['You are in the Bathroom. \nWest hallway is to the south.', None, None, 2, None]
+roomList.append(room)
+#WELDING
+room = ['You are in Welding. \nCentral hallway is to the south.', None, None,6, None]
+roomList.append(room)
+#AUTO
+room = ['You are in Auto Collision. \nEast hallway is to the south.', None, None, 4,  None]
+roomList.append(room)
+
+nextRoom = 0
+currentRoom = 0
+done = False
+while done == False:
+    print()
+    print(roomList[currentRoom][0])
+    dirChoice = input('What direction do you want to go? ')
+    if dirChoice == 'North':
+        if [currentRoom][1] == 2:
+            nextRoom = roomList[currentRoom][1]
+        elif [currentRoom][1] == None:
+            print("You can't go that way. ")
+        else:
+            currentRoom = nextRoom
+            
+    
