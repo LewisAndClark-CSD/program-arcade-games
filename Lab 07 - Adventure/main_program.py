db63397e1a79fd73a3f912e18a349455172eaa7c
Sthomp123 <sthompson@stcharlessd.org>
2017-11-29 11:12:48 -0600

    removed the hardcoded 'you are in' statements from room descriptions and put them in the while loop

--- Lab 07 - Adventure/main_program.py
+++ Lab 07 - Adventure/main_program.py
@@ -11,57 +11,57 @@ done = False
 #keeps track of your current room
 current_room = 0
 #This appends the room to a list that contains all the rooms
-room = ["You're standing on the great lawn of a gigantic castle.\nYou take your eyes off the castle for a second and notice a shed in the west. ", 1, None, None, 2]
+room = ["the great lawn of a gigantic castle.\nYou take your eyes off the castle for a second and notice a shed in the west. ", 1, None, None, 2]
 room_list.append(room)
 
-room = ["You're standing in the great entrance hall to the castle.\nThere is an alluring smell coming from the dining hall. ", 4, 3, 0, None]
+room = ["the great entrance hall to the castle.\nThere is an alluring smell coming from the dining hall. ", 4, 3, 0, None]
 room_list.append(room)
 
-room = ["You're in the castle's shed. There are many tools to keep the lawn tidy", None, 0, None, None]
+room = ["the castle's shed. There are many tools to keep the lawn tidy", None, 0, None, None]
 room_list.append(room)
 
-room = ["The cooks are preparing a delicious meal. You look past the table and see a hallway in front of you.", 5, None, None, 1]
+room = ["the dining room, the cooks are preparing a delicious meal. You look past the table and see a hallway in front of you.", 5, None, None, 1]
 room_list.append(room)
 
-room = ["You're in a long corridor that seems to only lead to another equally long corridor.", None, 5, 1, None]
+room = ["a long corridor that seems to only lead to another equally long corridor.", None, 5, 1, None]
 room_list.append(room)
 
-room = ["You can smell the meal coming from the dining room and see an entrance to a bedroom.", 6, None, 3, 4]
+room = ["a long corridor, you can smell the meal coming from the dining room and see an entrance to a bedroom.", 6, None, 3, 4]
 room_list.append(room)
 
-room = ["You have made it all the way to the kings spotless bedroom. It is the most amazing thing you have ever seen", None, None, 5, None]
+room = ["the kings bedroom, it is the most amazing thing you have ever seen", None, None, 5, None]
 room_list.append(room)
 #Main loop for the program
 while done == False:
     print()
     print("Press Q to quit at anytime")
     print()
-    print(room_list[current_room][0])
+    print("You are in", room_list[current_room][0])
     
     user_input = input("Which direction do you want to explore? ").lower()
     #Controls directions for north
-    if user_input == "north" or user_input == "n":
+    if user_input[0] == "n":
             next_room = room_list[current_room][1]
             if next_room == None:
                 print("You can't go that way.")
             else:
                 current_room = next_room            
     #Controls directions for eat
-    elif user_input == "east" or user_input == "e":
+    elif user_input[0] == "e":
         next_room = room_list[current_room][2]
         if next_room == None:
             print("You can't go that way.")
         else:
             current_room = next_room        
     #Controls directions for south
-    elif user_input == "south" or user_input == "s":
+    elif user_input[0] == "s":
         next_room = room_list[current_room][3]
         if next_room == None:
             print("You can't go that way.")
         else:
             current_room = next_room        
     #Controls directions for west
-    elif user_input == "west" or user_input == "w":
+    elif user_input[0] == "w":
         next_room = room_list[current_room][4]
         if next_room == None:
             print("You can't go that way.")
