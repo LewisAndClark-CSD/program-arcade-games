f908094030198ac5153fad9adc9e8ed0a47602db
RyanRosvall <rrosvall@stcharlessd.org>
2017-11-29 08:37:08 -0600

    Completed Chapter 7.

--- Lab 07 - Adventure/main_program.py
+++ Lab 07 - Adventure/main_program.py
@@ -18,7 +18,7 @@ room = ['You are in the Main Room. ', 1, 5, 3, 2]
 room_list.append(room)
 room = ['You are in the Window Room, overlooking the extravagant garden. ', None, None, 0, 2]
 room_list.append(room)
-room = ['You are in the Bedroom.', 1, 0, 7, None]
+room = ['You are in the Bedroom.', 1, 0, None, None]
 room_list.append(room)
 room = ['You are in the storage room, filled with surgical supplies.', 0, 5, 4, None]
 room_list.append(room)
@@ -28,27 +28,11 @@ room = ['You are in the lab, there is a dead man strapped to the table.', 6, Non
 room_list.append(room)
 room = ['You have found an empty room... bummer. Go back to see the dead man now.', None, None, 5, None]
 room_list.append(room)
-room = ['You Escaped the mansion! Great job!']
-room_list.append(room)
 
 while done == False:
+
     print(room_list[current_room][0])
     print()
-    nextMove = input("Which way would you like to go? ")
-    if nextMove.lower()[0] == "n":
-        next_room = (room_list[current_room][1])
-        current_room = next_room
-    elif nextMove.lower()[0] == "e":
-        next_room = (room_list[current_room][2])
-        current_room = next_room
-    elif nextMove.lower()[0] == "s":
-        next_room = (room_list[current_room][3])
-        current_room = next_room
-    elif nextMove.lower()[0] == "w":
-        next_room = (room_list[current_room][4])
-        current_room = next_room
-    elif nextMove.lower()[0] == "q":
-        break
     if room_list[current_room][1] != None:
         print("\tThere is an exit to the North.")
     if room_list[current_room][2] != None:
@@ -57,10 +41,23 @@ while done == False:
         print("\tThere is an exit to the South.")
     if room_list[current_room][4] != None:
         print("\tThere is an exit to the West.")
+    next_move = input("Which way would you like to go? ")
+    if next_move.lower()[0] == "n":
+        next_room = (room_list[current_room][1])
+    elif next_move.lower()[0] == "e":
+        next_room = (room_list[current_room][2])
+    elif next_move.lower()[0] == "s":
+        next_room = (room_list[current_room][3])
+    elif next_move.lower()[0] == "w":
+        next_room = (room_list[current_room][4])
+    elif next_move.lower()[0] == "q":
+        break
 
     if next_room == None:
         print("You can't go that way.")
         print()
     else:
         current_room = next_room
-        continue
+
+
+
